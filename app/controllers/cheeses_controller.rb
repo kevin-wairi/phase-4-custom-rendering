f14d144d5f967a6e4c19815e87607e66f1f1133c
kevin wairi <kevinwairi@gmail.com>
2023-06-20 02:54:24 +0300

    assignment completed

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -9,7 +9,11 @@ class CheesesController < ApplicationController
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
+    if cheese
     render json: cheese
+    else 
+      render json: {error: 'cheese not found'},status: :not_found
+    end
   end
 
 end
